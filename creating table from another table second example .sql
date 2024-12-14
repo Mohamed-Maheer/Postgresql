4319486c99c2fef940eed6e0fecb39d855574f15
mohamed Maher <mohamedmaherr460@gmail.com>
2024-12-14 19:23:58 +0200

    Delete creating table from another table second example .sql

--- creating table from another table second example .sql
+++
@@ -1,6 +0,0 @@
-create table feb as select * from job_postings where extract(year from job_posted_date)= 2023 
-and extract(month from job_posted_date )= 2;
-
-create table mar as select * from job_postings where extract(year from job_posted_date)= 2023 
-and extract(month from job_posted_date )= 3;
-
