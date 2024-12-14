d91627edfac4a11c73bc9d00f6e946c60179553a
mohamed Maher <mohamedmaherr460@gmail.com>
2024-12-14 19:20:32 +0200

    Delete creating table from another table.sql

--- creating table from another table.sql
+++
@@ -1,3 +0,0 @@
-create table jan_feb_mar as select * from job_postings where extract(year from job_posted_date)=2023 and extract(month from job_posted_date)=1 or extract(month from job_posted_date)=2 or extract(month from job_posted_date)=3;
-
- 
